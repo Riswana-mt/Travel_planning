e986dd9b3011f56275d90a7a8c7677b141d2b0ee
Riswana-mt <risbana187@gmail.com>
2025-05-20 21:57:54 +0530

    Update wsgi.py

--- Mapspark_project/Mapspark_project/wsgi.py
+++ Mapspark_project/Mapspark_project/wsgi.py
@@ -7,19 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import sys
-from pathlib import Path
-
-# Add project directory to Python path
-project_root = Path(__file__).resolve().parent.parent
-sys.path.append(str(project_root))
-
 
 
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+# Calculate paths
+BASE_DIR = Path(__file__).resolve().parent.parent
+SRC_DIR = BASE_DIR if (BASE_DIR / 'manage.py').exists() else BASE_DIR.parent
 
+# Add to Python path
+sys.path.append(str(SRC_DIR))
+
+# Set Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mapspark_project.settings')
 
+# Initialize application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
